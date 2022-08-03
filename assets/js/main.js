e77170a694c7bfad7da3da9eb80924631c782323
asdfjennifer <jennifergenger@gmailcom>
2022-08-03 14:31:09 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,35 @@
-// console.log("ey");
-
 let input = document.getElementById("message");
-let outputError = document.getElementById("output-error");
-let outputMessage = document.getElementById("output-message");
+// let outputError = document.getElementById("output-error");
 
-function submit() {
-    console.log("funzt");
-    // document.getElementById("message").value.innerHTML =
-    console.log(input.value);
-    outputError.innerHTML = "Test";
-    outputMessage.innerHTML = "Test";
-}
+document.getElementById('errorOutput').style.display = 'none';
+
+function submitInput() {
 
     if (input.value) {
-        outputMessage.innerHTML = "";
-        console.log("keine Nachricht erhalten");
-        outputMessage.innerHTML = input.value;
+        console.log("funzt");
+        document.getElementById("result").innerHTML = input.value;
     } else {
         console.log("keine Nachricht erhalten");
-        outputMessage.innerHTML = "Bitte gib eine Nachricht ein";
+        document.getElementById('errorOutput').style.display = 'block';
     }
+    // document.getElementById("message").value.innerHTML =
+    //console.log(input.value);
+    //outputError.innerHTML = "Test";
+    //outputMessage.innerHTML = "Test";
 
-    if (inputLength <= 0) {
-        console.log("keine Nachricht erhalten");
-    } else {
-        console.log("Nachricht erhalten");
-    }
+    // if (input.value) {
+    //     outputMessage.innerHTML = "";
+    //     console.log("keine Nachricht erhalten");
+    //     outputMessage.innerHTML = input.value;
+    // } else {
+    //     console.log("keine Nachricht erhalten");
+    //     outputMessage.innerHTML = "Bitte gib eine Nachricht ein";
+    // }
+
+    // if (input.value <= 0) {
+    //     console.log("keine Nachricht erhalten");
+    // } else {
+    //     console.log("Nachricht erhalten");
+    // }
+}
+
